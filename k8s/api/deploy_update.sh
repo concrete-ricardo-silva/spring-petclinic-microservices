afad345fedaba4303d56cfc6dc394ab311948775
[ricardo] <[ricardo6142@gmail.com]>
2018-03-29 05:08:57 -0300

    add update

--- k8s/api/deploy_update.sh
+++ k8s/api/deploy_update.sh
@@ -2,8 +2,6 @@
 PATH=$PATH:/var/jenkins_home/google-cloud-sdk/bin/
 gcloud auth activate-service-account --key-file=/var/jenkins_home/auth.json --project concrete-198923
 gcloud container clusters get-credentials petclinic-dev --zone us-central1-a
-kubectl set image deployment/api-gateway master=gcr.io/concrete-198923/spring-petclinic-api-gateway:$BUILD_NUMBER
-kubectl set image deployment/customers-service customers-service=gcr.io/concrete-198923/spring-petclinic-customers-service:$BUILD_NUMBER
-kubectl set image deployment/tracing-server tracing-server=gcr.io/concrete-198923/spring-petclinic-tracing-server:$BUILD_NUMBER
-kubectl set image deployment/vets-service vets-service=gcr.io/concrete-198923/spring-petclinic-vets-service:$BUILD_NUMBER
-kubectl set image deployment/visits-service visits-service=gcr.io/concrete-198923/spring-petclinic-visits-service:$BUILD_NUMBER
+APPS="spring-petclinic-customers-service spring-petclinic-tracing-server spring-petclinic-vets-service spring-petclinic-visits-service api-gateway"
+
+for app in $APPS; do kubectl set image deployment/$app $app=gcr.io/concrete-198923/spring-petclinic-$app:$BUILD_NUMBER; done
