774025dbf59ac1d6373c077cd5e6bef34600cc6f
concrete-ricardo-silva <37225962+concrete-ricardo-silva@users.noreply.github.com>
2018-03-29 00:50:23 -0300

    Update deploy.sh

--- k8s/api/deploy.sh
+++ k8s/api/deploy.sh
@@ -2,6 +2,7 @@
 PATH=$PATH:/var/jenkins_home/google-cloud-sdk/bin/
 gcloud auth activate-service-account --key-file=/var/jenkins_home/auth.json --project concrete-198923
 gcloud container clusters get-credentials petclinic-dev --zone us-central1-a
+cd ./k8s/api/
 kubectl create -f 1-config-server-deployment.yaml
 kubectl create -f 1-config-server-service.yaml
 kubectl create -f 2-discovery-server-deployment.yaml
