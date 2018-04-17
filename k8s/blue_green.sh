282fe469a4a726053bf8cfaebf2a9ec2ed98ed2c
concrete-ricardo-silva <37225962+concrete-ricardo-silva@users.noreply.github.com>
2018-04-16 22:53:28 -0300

    Update blue_green.sh

--- k8s/blue_green.sh
+++ k8s/blue_green.sh
@@ -26,7 +26,8 @@ cd $COLOR
 gcloud container clusters get-credentials petclinic-$COLOR-prod --zone us-central1-a
 
 # Remove deployment old versions
-kubectl delete  deployment admin-server  api-gateway   config-server  customers-service  discovery-server tracing-server   vets-service visits-service
+kubectl delete services,pods,deployment --all
+kubectl delete ingress api-gateway
 
 # Apply Deploy Tag
 for i in $(ls templates); do cat templates/$i | sed s/latest/$BUILD_NUMBER/g | sed 's/REPO/gcr.io\/acn-onboarding-devops/g'  >> $i; done
