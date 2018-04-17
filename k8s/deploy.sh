86dde454bcaf3c33f12f0b69fa82121f6392a0cf
[ricardo] <[ricardo6142@gmail.com]>
2018-04-17 15:33:42 -0300

    add blue green

---
+++ k8s/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Enter Env Dir
+
+cd ./k8s/$COLOR
+
+# Auth Cluster
+gcloud container clusters get-credentials petclinic-$COLOR-prod --zone us-central1-a
+
+# Remove deployment old versions
+kubectl delete services,pods,deployment --all
+kubectl delete ingress api-gateway
+
+# Apply Deploy Tag
+for i in $(ls templates); do cat templates/$i | sed s/latest/$BUILD_NUMBER/g | sed 's/REPO/gcr.io\/acn-onboarding-devops/g'  >> $i; done
+
+# Apply Deployment
+kubectl create -f .
+
+
---
+++ k8s/dns.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# change DNS
+
+gcloud dns record-sets transaction start -z=redligth
+
+gcloud dns record-sets transaction remove -z=redligth --type=CNAME --name="petclinicprod.redligth.com.br." --ttl 300 "petclinicblue.redligth.com.br."
+
+gcloud dns record-sets transaction remove -z=redligth --type=CNAME --name="petclinicprod.redligth.com.br." --ttl 300 "petclinicgreen.redligth.com.br."
+
+gcloud dns record-sets transaction add -z=redligth --type=CNAME --name="petclinicprod.redligth.com.br." --ttl 300 "petclinic$COLOR.redligth.com.br."
+gcloud dns record-sets transaction execute -z=redligth
+
+# update env
+
+echo $COLOR > state
+gsutil cp state gs://state-config/
+rm state
+rm *deployment*
+
