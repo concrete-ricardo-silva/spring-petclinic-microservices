dbebc893dcba0c198fc4b83bb501a2a58cee1eb2
[ricardo] <[ricardo6142@gmail.com]>
2018-04-16 21:47:25 -0300

    add blue green

---
+++ k8s/blue_green.sh
@@ -0,0 +1,49 @@
+# Get Color
+
+COLOR=$(gsutil cat  gs://state-config/state)
+
+if [ "$COLOR" = "blue" ]
+then
+    echo "Env At Blue, setting to green"
+    COLOR=green
+elif [ "$COLOR" = "green" ]
+then
+    echo "Env at Green, setting to blue"
+    COLOR=blue
+else
+    echo "Not Cool, no color defined"
+    exit 1
+fi 
+
+# Enter Env Dir
+
+cd $COLOR
+
+# Auth Cluster
+gcloud container clusters get-credentials petclinic-$COLOR-prod --zone us-central1-a
+
+# Remove deployment old versions
+# kubectl delete deployment iuiui iuiui iuiu iuiui 
+
+# Apply Deploy Tag
+for i in $(ls templates); do cat templates/$i | sed s/latest/$BUILD_NUMBER/g  >> $i; done
+
+# Apply Deployment
+kubectl create -f .
+
+# Waiting tests
+
+# change DNS
+
+gcloud dns record-sets transaction start -z=redligth
+gcloud dns record-sets transaction add -z=redligth --name="petclinicprod.redligth.com.br" --type=CNAME --ttl=300 "petclinic$COLOR.redligth.com.br"
+gcloud dns record-sets transaction execute -z=redligth
+
+
+# update env
+
+echo $COLOR > state
+gsutil cp state gs://state-config/
+rm state
+
+
