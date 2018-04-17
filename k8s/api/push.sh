e7a547b349f1de80b7ef60717a9df86c613a89c3
concrete-ricardo-silva <37225962+concrete-ricardo-silva@users.noreply.github.com>
2018-04-16 21:34:16 -0300

    Update push.sh

--- k8s/api/push.sh
+++ k8s/api/push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker login -u _json_key -p "$(cat /var/jenkins_home/auth.json )" https://gcr.io
-IMAGES="spring-petclinic-api-gateway spring-petclinic-customers-service spring-petclinic-tracing-server spring-petclinic-vets-service spring-petclinic-visits-service"
-for image in $IMAGES; do docker push gcr.io/concrete-198923/$image:$BUILD_NUMBER; done
+docker login -u _json_key -p https://gcr.io
+
+IMAGES="spring-petclinic-discovery-server spring-petclinic-config-server spring-petclinic-api-gateway spring-petclinic-customers-service spring-petclinic-tracing-server spring-petclinic-vets-service spring-petclinic-visits-service"
+
+for image in $IMAGES; do docker push $REPO/$image:$BUILD_NUMBER; done
